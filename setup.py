29d527947772d9f1eb14b5f5af90848d03b8c2d5
Adrian Fernandez <adrian@luengo.co>
2021-03-25 10:56:33 +0000

    Bump version and add project description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    readme = f.read()
+
 setup(
     name='supertouch',
     py_modules=['supertouch'],
-    version="0.1.0",
+    version="0.1.1",
     author_email="adrian@luengo.co",
-    license_files=("LICENSE.txt",),
+    license="MIT",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     python_requires=">3.6",
     description="Easily create files and folders.",
     entry_points={
